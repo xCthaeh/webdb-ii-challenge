8f32829b4a29843fd0de583662f5fe1a73cfeaaa
Alyssa Jane <xcthaeh@gmail.com>
2019-06-04 17:44:26 -0400

    created a put request for zoos

--- index.js
+++ index.js
@@ -27,6 +27,24 @@ server.get("/api/zoos/:id", (req, res) => {
     .catch(err => res.status(500).json({ error: err }));
 });
 
+server.put("/api/zoos/:id", (req, res) => {
+  const changes = req.body;
+  const zooId = req.params.id;
+  db("zoos")
+    .where({ id: zooId })
+    .update(changes)
+    .then(count => {
+      if (count) {
+        res.status(200).json({ count });
+      } else {
+        res.status(404).json({ error: "No zoo's with this ID could be found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
