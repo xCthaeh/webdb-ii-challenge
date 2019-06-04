c3acf371784797f2c2f9cdbafb6a02a8f6113f07
Alyssa Jane <xcthaeh@gmail.com>
2019-06-04 17:42:05 -0400

    added get requests for zoos

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.json({ api: "running" });
+});
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => res.status(200).json(zoos))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  const zooId = req.params.id;
+  db("zoos")
+    .where({ id: zooId })
+    .then(zooInfo => res.status(200).json(zooInfo))
+    .catch(err => res.status(500).json({ error: err }));
+});
 
 const port = 3300;
 server.listen(port, function() {
